66d3ee354b4c5ecf60c9e185882f894a7edbbdf5
Rpx1234 <22777764+Rpx1234@users.noreply.github.com>
2019-01-31 11:39:10 -0600

    Add files via upload

---
+++ Lab01a.java
@@ -0,0 +1,42 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class Lab01a
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+      StarsAndStripes flag = new StarsAndStripes();
+      //call the methods needed to make the patterns on the word document
+      flag.twentyDashes();
+      flag.twentyStars ();
+      flag.twentyDashes ();
+      flag.twentyStars ();
+      flag.twentyDashes ();
+      flag.twentyStars ();
+      flag.twentyDashes ();
+      flag.twoBlankLines ();
+      flag.twentyDashes ();
+      flag.twentyStars ();
+      flag.twentyDashes ();
+      flag.twentyStars ();
+      flag.twentyDashes ();
+      flag.twentyStars ();
+      flag.twentyDashes ();
+      flag.twentyDashes ();
+      flag.twentyStars ();
+      flag.twentyDashes ();
+      flag.twentyStars ();
+      flag.twentyDashes ();
+      flag.twentyStars ();
+      flag.twentyDashes ();
+      
+      
+           
+     
+   }
+}
---
+++ StarsAndStripes.java
@@ -0,0 +1,33 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsAndStripes
+{
+   public StarsAndStripes()
+   {
+     out.println("StarsAndStrips");
+     out.println("");
+     out.println("");
+   }
+
+   public void twentyStars()
+   {
+     out.println("********************");
+   }
+
+   public void twentyDashes()
+   {
+    out.println("-- -- -- -- -- -- -- -- -- --");
+   }
+
+   public void twoBlankLines()
+   {
+    out.println("");
+    out.println("");
+
+   }
+}
